7e26681c05365b12ff36829fdc5e783aeedfcbef
Siddhant Dhanaji Kadam <siddh4194@gmail.com>
2024-09-22 20:41:10 +0530

    showcase of work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HomePageComponent],
+  imports: [RouterOutlet, CommonModule, HomePageComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
