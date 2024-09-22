8033fbd7de7d25cee80e6e31e73602dd45b0d6f4
Siddhant Dhanaji Kadam <siddh4194@gmail.com>
2024-09-22 15:02:01 +0530

    gradient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,7 +18,6 @@ export class AppComponent {
   // Toggle function for the mobile menu
   toggleMenu() {
     console.log('its clicked');
-
     this.isMenuOpen = !this.isMenuOpen;
   }
 }
